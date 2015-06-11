af969eb6e13759e3808957b57fd1a6d8fef1f4a2
Ross Schmadebeck <ross.schmadebeck@gmail.com>
2015-06-10 20:34:26 -0400

    Confirming JS changes with 5 added president objects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 // Presidents Object
+var presidents = {
+	barackObama: {
+		firstName: 'Barack',
+		lastName: 'Obama',
+		termLength: '2',
+		party: 'Democratic',
+		yearsOfPresidency: '7',	
+	},
+	georgeBushJr: {
+		firstName: 'George',
+		lastName: 'Bush, Sr.',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8',
+	},
+	billClinton: {
+		firstName: 'William',
+		lastName: 'Clinton',
+		termLength: '2',
+		party: 'Democratic',
+		yearsOfPresidency: "8",
+	},
+	georgeBushSr: {
+		firstName: 'George',
+		lastName: 'Bush',
+		termLength: '1',
+		party: 'Republican',
+		yearsOfPresidency: '4',
+	},
+	ronaldReagan: {
+		firstName: 'Ronald',
+		lastName: 'Reagan',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8',
+	};
+}
